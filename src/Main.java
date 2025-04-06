88ca05930f0bed03d181c2cc42c0d8718a35fe85
aerobikViskon <aerobik.viskon1o@icloud.com>
2025-04-06 12:58:13 +0300

    Create Program

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        System.out.print("Matematik notunuzu giriniz: ");
+        mat = sc.nextInt();
+        System.out.print("Fizik notunuzu giriniz: ");
+        fizik = sc.nextInt();
+        System.out.print("Kimya notunuzu giriniz: ");
+        kimya = sc.nextInt();
+        System.out.print("Tarih notunuzu giriniz: ");
+        turkce = sc.nextInt();
+        System.out.print("Tarih notunuzu giriniz: ");
+        tarih = sc.nextInt();
+        System.out.print("Muzik notunuzu giriniz: ");
+        muzik = sc.nextInt();
+
+
+
+
+    }
+}
